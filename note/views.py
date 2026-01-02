30a25a5a874cd4d26b6e892e7f5a3cf84f5a6a40
giorgi <giorgimtsituri5@gmail.com>
2026-01-02 10:02:15 +0400

    add login logout signup

--- note/views.py
+++ note/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import generic
+from .forms import CustomerUserCreationForm
+from django.urls import reverse_lazy
 
-def home(requst):
-    return HttpResponse('Hello World')
+def home(request):
+    return render(request, 'home.html', {})
+
+class SignupPageView(generic.CreateView):
+    form_class = CustomerUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+    
--- server/settings.py
+++ server/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +127,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 
 
 
