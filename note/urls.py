359bf85b2bf4b1bd328048645fb0314902dc1d7f
giorgi <giorgimtsituri5@gmail.com>
2026-01-12 11:10:32 +0400

    add Edit and Deelte An search in google

--- note/urls.py
+++ note/urls.py
@@ -4,5 +4,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.SignupPageView.as_view(), name='signup'),
     path('form/', views.myform, name='myform'),
+    path('task/<int:pk>/', views.note_update_view, name='edit_task'),
+    path('task/delete/<int:pk>/', views.note_delete_view, name='product_delete'),
+
    
 ]
--- note/views.py
+++ note/views.py
@@ -1,34 +1,58 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
 from .forms import CustomerUserCreationForm, TodoForm
 from django.urls import reverse_lazy
 from .models import Todo
 
 def home(request):
-    Todos = Todo.objects.all()
+    todos = Todo.objects.all()
     form = TodoForm()
-    if request.method == 'POST':
-        form = TodoForm(request.POST)
+    if request.method == "POST":
+        form = TodoForm(request.POST or None)
         if form.is_valid():
             form.save()
-        form = TodoForm()
-    context = { 
-                'form': form,
-                'todos': Todos
-               }
-    return render(request, 'home.html', context)
+            task = form.cleaned_data['task']
+            describtion = form.cleaned_data['describtion']
+            print('task', task)
+            print('describtion', describtion)
+            form = TodoForm()
+        return redirect('home')
+
+    context = {
+        "todos": todos,
+         "form": form 
+    }
+    return render(request,"home.html", context)
+
+
+def note_update_view(request, pk):
+    obj = get_object_or_404(Todo, pk=pk)
+    form = TodoForm(request.POST or None, instance=obj)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=obj)
+        if form.is_valid():
+            form.save() 
+        return redirect('home')
+    return render(request, 'task_edit.html', { 'form': form })
+
+def note_delete_view(request, pk):
+    obj = get_object_or_404(Todo, pk=pk)
+    if request.method == "POST":
+        obj.delete()
+        return redirect('/')
+    context = {
+        'object': obj
+    }
+    return render(request, 'delete_task.html', context)
+
+
+
 
 class SignupPageView(generic.CreateView):
     form_class = CustomerUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
-    
-    
-# class FormlistView(generic.ListView):
-#     model = Todo
-#     template_engine = 'from.html'
-    
+     
 def myform(request):
     Todos = Todo.objects.all()
     context = {
