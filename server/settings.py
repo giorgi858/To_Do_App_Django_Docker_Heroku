73f3ffe3b1d961b6704ade6b87742774e1ab770f
giorgi <giorgimtsituri5@gmail.com>
2026-03-25 09:49:27 +0400

    add final

--- server/settings.py
+++ server/settings.py
@@ -1,15 +1,57 @@
 from pathlib import Path
-import os 
-from dotenv import load_dotenv
-from environs import Env
+import os
 from datetime import timedelta
+from environs import Env
+import dj_database_url
 from django.utils.translation import gettext_lazy as _
-from celery.schedules import crontab
 
 env = Env()
 env.read_env()
 
-load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# -----------------------
+# SECURITY
+# -----------------------
+
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
+
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS",
+    default=[".herokuapp.com"]
+)
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+SECURE_SSL_REDIRECT = env.bool(
+    "DJANGO_SECURE_SSL_REDIRECT",
+    default=not DEBUG
+)
+
+SESSION_COOKIE_SECURE = env.bool(
+    "DJANGO_SESSION_COOKIE_SECURE",
+    default=not DEBUG
+)
+
+CSRF_COOKIE_SECURE = env.bool(
+    "DJANGO_CSRF_COOKIE_SECURE",
+    default=not DEBUG
+)
+
+SECURE_HSTS_SECONDS = env.int(
+    "DJANGO_SECURE_HSTS_SECONDS",
+    default=2592000
+)
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+
+# -----------------------
+# I18N
+# -----------------------
 
 LANGUAGE_CODE = "en"
 
@@ -17,232 +59,184 @@ LANGUAGES = [
     ("en", _("English")),
     ("ka", _("Georgian")),
 ]
+
 TIME_ZONE = "UTC"
+
 USE_I18N = True
 USE_TZ = True
-
-
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOCALE_PATHS = [
     BASE_DIR / "locale",
 ]
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG", default=False)
-
-ALLOWED_HOSTS = ['*']
-
-# Application definition
+# -----------------------
+# APPS
+# -----------------------
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
-    'note',
-    'api',
-    'corsheaders',
-    'crispy_forms',
-    'crispy_bootstrap5',   
-    'allauth',
-    'allauth.account',
-    'rest_framework',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
+
+    "django.contrib.sites",
+
+    "corsheaders",
+
+    "crispy_forms",
+    "crispy_bootstrap5",
+
+    "rest_framework",
+
+    "allauth",
+    "allauth.account",
+
     "django_celery_beat",
-    'django.contrib.sites',
-    'djcelery_email',
-      
+    "djcelery_email",
+
+    "note",
+    "api",
 ]
 
-AUTHENTICATION_BACKENDS = [
-    # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
-    # `allauth` specific authentication methods, such as login by email
-    'allauth.account.auth_backends.AuthenticationBackend',
-]
-# ACCOUNT_LOGOUT_ON_GET = False 
-ACCOUNT_SESSION_REMEMBER = True
-LOGIN_REDIRECT_URL = 'home'
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
 
 SITE_ID = 1
-# settings.py
-ACCOUNT_FORMS = {
-    'login': 'note.forms.CustomLoginForm',
-}
-    
-# django-crispy-forms
-CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
+# -----------------------
+# MIDDLEWARE
+# -----------------------
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware", # new
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',  
-    
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+
+    "corsheaders.middleware.CorsMiddleware",
+
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
     "allauth.account.middleware.AccountMiddleware",
 ]
 
-ROOT_URLCONF = 'server.urls'
+
+# -----------------------
+# TEMPLATES
+# -----------------------
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'server.wsgi.application'
+
+ROOT_URLCONF = "server.urls"
+WSGI_APPLICATION = "server.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        "NAME": 'd6128ma954h3uo',
-        "USER": 'ufm0i1vdjab0bk',
-        "PASSWORD": 'p109c77a19788eaf518be93b59b7f0c95b08efe13154dfae3853413300536b6a2',
-        "HOST": 'chepvbj2ergru.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
-        "PORT": '5432',
-    }}
+# -----------------------
+# DATABASE
+# -----------------------
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PWD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }}
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True,
+        default=os.getenv("DATABASE_URL")
+    )
+}
 
+# -----------------------
+# STATIC
+# -----------------------
 
-# DB_NAME=''
-# DB_USER=''
-# DB_PORT=''
-# DB_HOST=''
-# DB_PWD=''
-# EMAIL_HOST_USER=''
-# EMAIL_HOST_PASSWORD=''
+STATIC_URL = "/static/"
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         "NAME": os.getenv("DB_NAME"),
-#         "USER": os.getenv("DB_USER"),
-#         "PASSWORD": os.getenv("DB_PWD"),
-#         "HOST": os.getenv("DB_HOST"),
-#         "PORT": os.getenv("DB_PORT"),
-#     }}
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         "NAME": 'd6128ma954h3uo',
-#         "USER": 'ufm0i1vdjab0bk',
-#         "PASSWORD": 'p109c77a19788eaf518be93b59b7f0c95b08efe13154dfae3853413300536b6a2',
-#         "HOST": 'chepvbj2ergru.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
-#         "PORT": '5432',
-#     }}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"] # new
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-ACCOUNT_SIGNUP_FIELDS = [
-    'email*',
-    'username*',
-    'password1*',
-    'password2*',
+
+STATICFILES_STORAGE = (
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+)
+
+
+# -----------------------
+# AUTH
+# -----------------------
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
+
+LOGIN_REDIRECT_URL = "home"
+
+ACCOUNT_SESSION_REMEMBER = True
 
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 
-# If we want not to use username at all.
-# ACCOUNT_SIGNUP_FIELDS = [
-#     'email*',
-#     'password1*',
-#     'password2*',
-# ]
-# ACCOUNT_USERNAME_REQUIRED = False   # optional, but common
-# ACCOUNT_USER_MODEL_USERNAME_FIELD = None
-# ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_ADAPTER = "note.adapter.CeleryAccountAdapter"
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWS_CREDENTIALS = True
+ACCOUNT_SIGNUP_FIELDS = [
+    "email*",
+    "username*",
+    "password1*",
+    "password2*",
+]
 
+
+# -----------------------
+# EMAIL
+# -----------------------
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+
+# -----------------------
+# CORS
+# -----------------------
+
+CORS_ALLOW_ALL_ORIGINS = env.bool(
+    "CORS_ALLOW_ALL_ORIGINS",
+    default=False
+)
+
+
+# -----------------------
+# DRF + JWT
+# -----------------------
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
@@ -256,21 +250,32 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(minutes=30),
 }
 
+
+# -----------------------
+# SESSION
+# -----------------------
+
 SESSION_COOKIE_AGE = 60 * 60
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_SAVE_EVERY_REQUEST = True
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# -----------------------
+# CELERY
+# -----------------------
 
-# Celery Settings
-CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_BROKER_URL = os.getenv(
+    "REDIS_URL",
+    "redis://redis:6379/0"
+)
+
+CELERY_RESULT_BACKEND = os.getenv(
+    "REDIS_URL",
+    "redis://redis:6379/0"
+)
+
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_BACKEND = "redis://redis:6379/0"
-
 
 
 CELERY_BEAT_SCHEDULE = {
@@ -279,12 +284,10 @@ CELERY_BEAT_SCHEDULE = {
         "schedule": 60.0,
     },
 }
-ACCOUNT_ADAPTER = 'note.adapter.CeleryAccountAdapter'
 
-SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
-SECURE_HSTS_SECONDS = env.int("DJANGO_SECURE_HSTS_SECONDS", default=2592000)
-SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True)
-SECURE_HSTS_PRELOAD= env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
-SESSION_COOKIE_SECURE=env.bool("DJANGO_SESSION_COOKIE_SECURE", default=True)
-CSRF_COOKIE_SECURE=env.bool("DJANGO_CSRF_COOKIE_SECURE", default=True)
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# -----------------------
+# DEFAULT PK
+# -----------------------
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
